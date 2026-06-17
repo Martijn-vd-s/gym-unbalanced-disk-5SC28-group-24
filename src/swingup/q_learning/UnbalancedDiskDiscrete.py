be5e68c2b8deafaa7c5b8cd50abef12e8ae2ab0c
Josha <j.j.j.kuipers@student.tue.nl>
2026-06-17 10:46:21 +0200

    45 new working with system I but without top punishment

--- src/swingup/q_learning/UnbalancedDiskDiscrete.py
+++ src/swingup/q_learning/UnbalancedDiskDiscrete.py
@@ -16,13 +16,13 @@ class UnbalancedDisk(gym.Env):
                     0  = starting location
     '''
     def __init__(self, umax=3., dt = 0.025, render_mode='human'):
-        # ############# start do not edit  ################
-        self.omega0 = 11.339846957335382
-        self.delta_th = 0
-        self.gamma = 1.3328339309394384
-        self.Ku = 28.136158407237073
-        self.Fc = 6.062729509386865
-        self.coulomb_omega = 0.001
+        # # ############# start do not edit  ################
+        # self.omega0 = 11.339846957335382
+        # self.delta_th = 0
+        # self.gamma = 1.3328339309394384
+        # self.Ku = 28.136158407237073
+        # self.Fc = 6.062729509386865
+        # self.coulomb_omega = 0.001
 
         # # self.g = 9.80155078791343
         # # self.J = 0.000244210523960356
@@ -31,27 +31,27 @@ class UnbalancedDisk(gym.Env):
         # # self.M = 0.0761844495320390
         # # self.tau = 0.397973147009910
         # ############# end do not edit ###################
-        # self.omega0 = 12.7908
-        # self.delta_th = 0
-        # self.gamma = 2.1904
-        # self.Ku = 30.4070
-        # self.Fc = 9.1626
-        # self.coulomb_omega = 0.001
+        self.omega0 = 12.7908
+        self.delta_th = 0
+        self.gamma = 2.1904
+        self.Ku = 30.4070
+        self.Fc = 9.1626
+        self.coulomb_omega = 0.001
 
 
 
         self.umax = umax
         self.dt = dt #time step
-        self.num_actions = 9
+        self.num_actions = 7
         self.render_mode = render_mode
 
         self.action_space = spaces.Discrete(self.num_actions)
         # Fijne resolutie rond 0 voor zacht vasthouden bij de top (vult het gat tussen 0 en 0.5).
         # holdhoek per koppel (nieuwe params): 0.15->1.6deg, 0.35->3.7deg, 0.7->7.5deg, 1.8->20deg, 3->34deg
         # self.discrete_action_map  = [-3, -1.8, -0.7, -0.35, -0.15, 0, 0.15, 0.35, 0.7, 1.8, 3] #fine
-        # self.discrete_action_map  = [-3, -1.8,  -0.5 ,  0,  0.5, 1.8,  3] #1
+        self.discrete_action_map  = [-3, -1.8,  -0.7 ,  0,  0.7, 1.8,  3] #1
         # self.discrete_action_map  = [-3, -1.2 ,  0, 1.2,  3] #1
-        self.discrete_action_map  = [-3,  -2, -1,  -0.6 , 0,  0.6, 1, 2, 3] #2
+        # self.discrete_action_map  = [-3,  -2, -1,  -0.6 , 0,  0.6, 1, 2, 3] #2
         # self.discrete_action_map  = [-3,  -1.7, -0.7,  -0.2, 0,  0.2, 0.7, 1.7, 3] #3
         low = [-(5/4)*np.pi,-5] 
         high = [(5/4)*np.pi,5]
@@ -114,9 +114,9 @@ class UnbalancedDisk(gym.Env):
 
 
         # --- Sim-to-real shaping: zacht en glad sturen vlak bij de top ---
-        self.TOP_GATE_SIGMA = 0.45  # rad (~26 deg): breedte van de "bij de top" zone (breder -> eerder zacht)
-        self.W_U_TOP = 0.40         # extra straf op u^2 vlak bij de top (hoger -> minder voltage aan de top)
-        self.W_RATE_TOP = 0.05      # straf op snelle koppelwisselingen (anti-chatter) bij de top
+        self.TOP_GATE_SIGMA = 0.3  # rad (~26 deg): breedte van de "bij de top" zone (breder -> eerder zacht)
+        self.W_U_TOP = 0.10         # extra straf op u^2 vlak bij de top (hoger -> minder voltage aan de top)
+        self.W_RATE_TOP = 0.01      # straf op snelle koppelwisselingen (anti-chatter) bij de top
         # gate ~1 bij de top, ~0 elders -> shaping alleen waar fijn balanceren nodig is
         self.top_gate = lambda: np.exp(-0.5 * (self.err(self.th, np.pi) / self.TOP_GATE_SIGMA) ** 2)
 
@@ -133,10 +133,10 @@ class UnbalancedDisk(gym.Env):
             + gaussian_2d(self_instance.err(self_instance.th, np.pi), self_instance.omega, 0, 0, 0.07, 0.07, 0.0, 0.004)
 
 
-            # Control input penalty: klein tijdens swing-up, groot bij de top (top_gate)
-            - (0.01 + self_instance.W_U_TOP * self_instance.top_gate()) * self_instance.u**2
-            # Action-rate penalty (anti-chatter), alleen bij de top zodat pompen tijdens swing-up vrij blijft
-            - self_instance.W_RATE_TOP * self_instance.top_gate() * (self_instance.u - self_instance.prev_u)**2
+            # # Control input penalty: klein tijdens swing-up, groot bij de top (top_gate)
+            # - (0.01 + self_instance.W_U_TOP * self_instance.top_gate()) * self_instance.u**2
+            # # Action-rate penalty (anti-chatter), alleen bij de top zodat pompen tijdens swing-up vrij blijft
+            # - self_instance.W_RATE_TOP * self_instance.top_gate() * (self_instance.u - self_instance.prev_u)**2
 
             # TOEVOEGING van de getransformeerde Z_VALUES
             # Aangepaste aanroep: self_instance.calculate_z_component() in plaats van self_instance.err.calculate_z_component()
@@ -220,7 +220,7 @@ class UnbalancedDisk(gym.Env):
          
     def reset(self,seed=None):
         super().reset(seed=seed)
-        self.th = self.set_th if self.set_th is not None else np.random.uniform(-np.pi*(2/3), np.pi*(2/3))
+        self.th = self.set_th if self.set_th is not None else np.random.uniform(-np.pi*(4/5), np.pi*(4/5))
         self.omega = self.set_omega if self.set_omega is not None else np.random.uniform(-2.8, 2.8)
         self.u = 0
         self.prev_u = 0
